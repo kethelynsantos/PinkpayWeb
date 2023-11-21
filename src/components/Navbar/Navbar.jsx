df878d7786811e35db040cd2783aeefddd092ab0
kethelynsantos <kethelyngabrielly14@gmail.com>
2023-11-20 22:51:27 -0300

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import MobileNavbar from './MobileNavbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 function Navbar() {
+  useEffect(() => {
+    const mobileNavbar = new MobileNavbar(".mobile-menu", ".nav-list", ".nav-list li");
+    mobileNavbar.init();
+  }, []);
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
           <h1 className='logo'>Pinkpay</h1>
 
+          <div className="mobile-menu">
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+
           <div className="menu-responsive">
-            <ul className='menu-list'>
+            <ul className='nav-list'>
               <li>
                 <Link to="/">Inicio</Link>
               </li>
@@ -26,16 +38,16 @@ function Navbar() {
                 <Link to="/contato">Contato</Link>
               </li>
             </ul>
-          
+
 
             <div className="buttons-navbar">
               <button className='button-register'>Cadastrar</button>
               <button className='button-nav'>Entrar</button>
             </div>
 
-            
+
           </div>
-          
+
           <div className="nav-icon-container">
             <FontAwesomeIcon icon={faBars} />
           </div>
