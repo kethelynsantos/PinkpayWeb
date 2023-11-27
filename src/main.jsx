360ae5abeb95398453880e6e084aa713ff02a1e1
kethelynsantos <kethelyngabrielly14@gmail.com>
2023-11-27 10:51:32 -0300

    update in connection with the api

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { UserProvider } from './UserContext'
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
--- src/pages/LoginRegistration/LoginRegistration.jsx
+++ src/pages/LoginRegistration/LoginRegistration.jsx
@@ -1,7 +1,110 @@
 import React, { useEffect, useState } from 'react';
+import { useUser } from '../../UserContext';
+import axiosInstance from '../../sevices/axiosInstance';
 import './LoginRegistration.css';
 
 function LoginRegistration() {
+  const { state, dispatch } = useUser();
+
+  const [cpf, setCPF] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    console.log('Token atual:', state.token);
+  }, [state.token]);
+
+  const createUserAndLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Criação do usuário
+      const response = await axiosInstance.post(
+        'customuser/register/',
+        {
+          cpf: cpf,
+          password: password,
+        },
+        {
+          headers: {
+            'Authorization': 'Token fd478484c6a5192225e424c463b114ffe5143a30',
+          },
+        }
+      );
+
+      console.log('Usuário criado com sucesso:', response.data);
+
+      // Atualiza o token no estado global
+      dispatch({ type: 'SET_TOKEN', payload: response.data.token });
+
+      // Realiza automaticamente o login após criar o usuário
+      await loginUser();
+
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error.response.data);
+    }
+  };
+
+  const loginUser = async () => {
+    try {
+      const login = await axiosInstance.post(
+        'auth/token/login/',
+        {
+          cpf: cpf,
+          password: password,
+        },
+        {
+          headers: {
+            'Authorization': `Token fd478484c6a5192225e424c463b114ffe5143a30`,
+          },
+        }
+      );
+
+      // Atualiza o token no estado global
+      dispatch({ type: 'SET_TOKEN', payload: login.data.auth_token });
+
+    } catch (error) {
+      console.error('Erro ao fazer login:', error.response.data);
+    }
+  };
+
+  const createClient = async (event) => {
+    event.preventDefault();
+
+    try {
+      const token = state.token;
+
+      const response = await axiosInstance.post(
+        'client/',
+        {
+          name: name,
+          birth_date: birthdate,
+          phone: phoneNumber,
+          email: email,
+        },
+        {
+          headers: {
+            'Authorization': `Token ${token}`,
+          },
+        }
+      );
+
+      console.log('Cliente criado com sucesso:', response.data);
+
+      alert('Cliente criado com sucesso!');
+
+    } catch (error) {
+      if (error.response) {
+        console.error('Erro ao criar cliente:', error.response.data);
+      } else {
+        console.error('Erro ao criar cliente:', error.message);
+      }
+    }
+  };
+
   useEffect(() => {
     const btnSignin = document.querySelector("#signin");
     const btnSignup = document.querySelector("#signup");
@@ -25,59 +128,32 @@ function LoginRegistration() {
     };
   }, []);
 
-  const [cpf, setCPF] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSignup = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await fetch('api/v1/client', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          cpf,
-          password,
-        }),
-      });
-
-      if (response.ok) {
-        console.log('Usuário cadastrado com sucesso!');
-        // Adicione o redirecionamento ou qualquer outra lógica após o cadastro bem-sucedido.
-      } else {
-        console.error('Erro ao cadastrar usuário.');
-      }
-    } catch (error) {
-      console.error('Erro ao realizar a solicitação:', error);
-    }
-  };
-
   return (
     <div className="container-login">
       {/* Conteúdo da primeira seção */}
       <div className="content first-content">
         <div className="first-column">
-          <h2 className="title title-primary">Bem vindo de volta!</h2>
-          <p className="description description-primary">Para se manter conectado conosco</p>
-          <p className="description description-primary">por favor faça login com os seus dados</p>
-          <button id="signin" className="btn btn-primary">Entrar</button>
+          <h2 className="title title-primary">Realize seu cadastro!</h2>
+          <p className="description description-primary"> Realize seu cadastro e comece já essa aventura</p>
+          <p className="description description-primary">cor de rosa com o melhor banco do Brasil</p>
+          <button id="signin" className="btn btn-primary">Cadastrar</button>
         </div>
         <div className="second-column">
           <h2 className="title title-second">Crie sua conta</h2>
           <p className="description description-second">Preencha os campos e solicite sua conta Pinkpay</p>
-          <form className="form" onSubmit={handleSignup}>
-            <label className="label-input" htmlFor="">
+          <form className="form" onSubmit={createUserAndLogin}>
+            <label className="label-input" htmlFor="cpf">
               <input
+                id="cpf"
                 type="text"
                 placeholder="Digite seu CPF"
                 value={cpf}
                 onChange={(e) => setCPF(e.target.value)}
               />
             </label>
-            <label className="label-input" htmlFor="">
+            <label className="label-input" htmlFor="password">
               <input
+                id="password"
                 type="password"
                 placeholder="Crie uma senha"
                 value={password}
@@ -85,7 +161,7 @@ function LoginRegistration() {
               />
             </label>
             <button type="submit" className="btn btn-second">
-              Cadastre-se
+              Criar
             </button>
           </form>
         </div>
@@ -97,20 +173,49 @@ function LoginRegistration() {
           <h2 className="title title-primary">Olá, pessoa!</h2>
           <p className="description description-primary">Entre com os seus dados e abra</p>
           <p className="description description-primary">as portas para um mundo financeiro Pink</p>
-          <button id="signup" className="btn btn-primary">Cadastre-se</button>
+          <button id="signup" className="btn btn-primary">Criar conta</button>
         </div>
         <div className="second-column">
-          <h2 className="title title-second">faça login</h2>
+          <h2 className="title title-second">cadastre-se</h2>
           <p className="description description-second">e comece agora a desfrutar desse mundo Pink</p>
-          <form className="form">
-            <label className="label-input" htmlFor="">
-              <input type="email" placeholder="Digite seu CPF" />
+          <form className="form" onSubmit={createClient}>
+            <label className="label-input" htmlFor="name">
+              <input
+                id="name"
+                type="text"
+                placeholder="Digite seu nome completo"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </label>
-            <label className="label-input" htmlFor="">
-              <input type="password" placeholder="Digite sua senha" />
+            <label className="label-input" htmlFor="birthdate">
+              <input
+                id="birthdate"
+                type="text"
+                placeholder="Digite sua data de nascimento (YYYY-MM-DD)"
+                value={birthdate}
+                onChange={(e) => setBirthdate(e.target.value)}
+              />
             </label>
-            <a className="password" href="#">Esqueci minha senha</a>
-            <button className="btn btn-second">Entrar</button>
+            <label className="label-input" htmlFor="phoneNumber">
+              <input
+                id="phoneNumber"
+                type="text"
+                placeholder="Digite seu número de celular"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </label>
+            <label className="label-input" htmlFor="email">
+              <input
+                id="email"
+                type="text"
+                placeholder="Digite seu e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <button className="btn btn-second">Cadastrar</button>
           </form>
         </div>
       </div>
